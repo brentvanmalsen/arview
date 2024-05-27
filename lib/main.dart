fb334bd1b7f7843cef5d446c6ae06ee95efaf994
Brent van Malsen <brentvanmalsen@gmail.com>
2024-05-27 23:58:12 +0200

    Feature: Added ARKitNode with 3D bird model in the sphere

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:vector_math/vector_math_64.dart';
 
 void main() {
@@ -7,31 +7,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Test',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter ARKit Bird',
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -40,41 +29,25 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: ARKitSceneView(
-          onARKitViewCreated: (controller) => arView(controller),
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+      body: ARKitSceneView(
+        onARKitViewCreated: (controller) => arView(controller),
+      ),
+    );
   }
 }
 
 void arView(ARKitController controller) {
+  final birdModelPath = "objects/bird.obj";
+
   final nodeAr = ARKitNode(
-    geometry: ARKitSphere(
-      materials: [
-        ARKitMaterial(
-          diffuse: ARKitMaterialProperty.image("images/image.jpg"),
-          doubleSided: true,
-        ),
-      ],
-      radius: 1
+    geometry: ARKitReferenceNode(
+      url: birdModelPath,
+      scale: Vector3.all(0.1),
     ),
-    position: Vector3(0,0,0),
+    position: Vector3(0, 0, -1),
+    eulerAngles: Vector3.zero(),
   );
+
   controller.add(nodeAr);
 }
